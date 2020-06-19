ad05ee020f5a21b394bb71a4f13f329a04a71d49
Stanislaw Chirko <post.chirko@gmail.com>
2020-06-19 12:32:13 +0200

    добавляет выполнение задания Учебный проект: pimp my mage

--- js/dialog.js
+++ js/dialog.js
@@ -1,11 +1,46 @@
 'use strict';
 
 (function () {
+  var MIN_NAME_LENGTH = 2;
+  var MAX_NAME_LENGTH = 25;
   var userDialog = document.querySelector('.setup');
   var userDialogOpen = document.querySelector('.setup-open-icon');
   var userDialogClose = userDialog.querySelector('.setup-close');
   var userNameInput = userDialog.querySelector('.setup-user-name');
   var dialogHandle = userDialog.querySelector('.upload');
+
+  var MAX_SIMILAR_WIZARD_COUNT = 4;
+
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+  var setupWizard = document.querySelector('.setup-wizard');
+  var setupWizardCoatColor = setupWizard.querySelector('.wizard-coat');
+  var setupWizardEyesColor = setupWizard.querySelector('.wizard-eyes');
+  var setupWizardFireballColor = document.querySelector('.setup-fireball');
+  var inputCoatValue = document.querySelector('input[name="coat-color"]');
+  var inputEyesValue = document.querySelector('input[name="eyes-color"]');
+  var inputFireballValue = document.querySelector('input[name="fireball-color"]');
+
+  var form = userDialog.querySelector('.setup-wizard-form');
+
+  var fragment = document.createDocumentFragment();
+
+  /**
+   * Отрисовывает карточку с магом
+   * @param {array} wizard
+   *
+   * @return {array} wizardElement
+   */
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  };
 
   var onPopupEscPress = function (evt) {
     if (!userNameInput.focused) {
@@ -93,4 +128,69 @@
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   });
+
+  userNameInput.addEventListener('input', function () {
+    var valueLength = userNameInput.value.length;
+
+    if (valueLength < MIN_NAME_LENGTH) {
+      userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+    } else if (valueLength > MAX_NAME_LENGTH) {
+      userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MIN_NAME_LENGTH) + ' симв.');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  var successHandler = function (wizards) {
+    for (var i = 0; i < MAX_SIMILAR_WIZARD_COUNT; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+
+    similarListElement.appendChild(fragment);
+
+    document.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var successMessage = function (message) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: green;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = message;
+    document.body.insertAdjacentElement('afterbegin', node);
+
+    // userDialog.classList.add('hidden');
+  };
+
+  var errorHandler = function (message) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = message;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
+
+  var submitHandler = function (evt) {
+    window.backend.save(new FormData(form), errorHandler, function () {
+
+      userDialog.classList.add('hidden');
+    });
+    evt.preventDefault();
+  };
+
+  form.addEventListener('submit', submitHandler);
+
+
+  window.colorize(setupWizardCoatColor, window.wizardsData.COAT_COLOR, inputCoatValue);
+  window.colorize(setupWizardEyesColor, window.wizardsData.EYES_COLOR, inputEyesValue);
+  window.colorize(setupWizardFireballColor, window.wizardsData.FIREBALL_COLOR, inputFireballValue);
 })();
